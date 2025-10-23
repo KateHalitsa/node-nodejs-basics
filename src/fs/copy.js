a26e2e9aabb1f11a5fc5e1ae234d07f5b4094231
Katya <kate.halitsa@gmail.com>
2025-10-23 20:57:22 +0300

    feat: implementation of the copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { promises as fs } from 'fs';
+import path from 'path';
 const copy = async () => {
-  // Write your code here
+    const sourceDir = path.join('src', 'fs', 'files');
+    const destDir = path.join('src', 'fs','files_copy');
+    try {
+        await fs.access(sourceDir);
+        await fs.access(destDir);
+        throw new Error('FS operation failed');
+    } catch (err) {
+        if (err.code !== 'ENOENT') {
+            throw err;
+        }
+        await fs.mkdir(destDir);
+        await fs.cp(sourceDir, destDir, { recursive: true });
+    }
 };
 
 await copy();
