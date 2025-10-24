94ab131b43fecbf0619b1ef8bff9f558e1df541a
Katya <kate.halitsa@gmail.com>
2025-10-24 19:59:58 +0300

    feat: implementation of the compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import zlib from 'zlib';
 const compress = async () => {
-  // Write your code here
-};
+    const input = fs.createReadStream('./src/zip/files/fileToCompress.txt');
+    const output = fs.createWriteStream('./src/zip/files/archive.gz');
+    const gzip = zlib.createGzip();
+
+    input.pipe(gzip).pipe(output);
+    output.on('error', () => {
+        throw new Error('FS operation failed');
+    });};
 
 await compress();
