6f88cffbf745056462f2f44fe0bc907fb0cd42ef
Katya <kate.halitsa@gmail.com>
2025-10-24 17:32:24 +0300

    feat: implementation of the calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-  // Write your code here
+    const filePath = path.resolve('./src/hash/files/fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(filePath);
+
+    stream.on('data', data => hash.update(data));
+    stream.on('end', () => console.log(hash.digest('hex')));
+    stream.on('error', () => {
+        throw new Error('FS operation failed');
+    });
 };
 
 await calculateHash();
